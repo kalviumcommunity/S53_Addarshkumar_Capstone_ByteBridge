9783cc09f5e3e06aa2993dffd60b6f95a6b0203a
AddarshKumar123 <addarshkumar2005@gmail.com>
2024-03-14 14:42:33 +0530

    updated server

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,12 @@
 const express=require("express");
 const app=express();
-const port=4000;
+require("dotenv").config();
+const port=process.env.PORT||5000;
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('error');
+});
 
 app.get("/",(req,res)=>{
     res.send("hello world");
