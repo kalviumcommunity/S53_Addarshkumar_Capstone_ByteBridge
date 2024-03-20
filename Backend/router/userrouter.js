f61f6e4562571a3965325af0c896f924f70c0302
AddarshKumar123 <addarshkumar2005@gmail.com>
2024-03-20 17:35:30 +0530

    updated post api

--- Backend/router/userrouter.js
+++ Backend/router/userrouter.js
@@ -4,6 +4,6 @@ const userRouter=express.Router();
 const {createUser,findUser}=require("../controller/usercontroller");
 
 userRouter.post("/signup",createUser);
-userRouter.get("/login",findUser)
+userRouter.post("/login",findUser)
 
 module.exports=userRouter;
