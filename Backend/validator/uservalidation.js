3583f2da3fe6fd002303576ccd7307ce4b1507cc
AddarshKumar123 <addarshkumar2005@gmail.com>
2024-04-23 15:41:21 +0530

    file upload functality and mapping

--- Backend/validator/uservalidation.js
+++ Backend/validator/uservalidation.js
@@ -3,7 +3,7 @@ const joi=require("joi");
 const userValidator= joi.object({
     name:joi.string().min(3).max(20).required(),
     email:joi.string().email().required(),
-    password:joi.string().pattern(new RegExp('^(?=.*[a-zA-Z])(?=.*[^a-zA-Z\d\s])(.{6,30})$')).required()
+    password:joi.string().pattern(new RegExp('^(?=.*[a-zA-Z])(?=.*[^a-zA-Z\d\s])(.{6,30})$'))
 })
 
 module.exports=userValidator
