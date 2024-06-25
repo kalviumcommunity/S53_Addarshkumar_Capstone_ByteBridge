792780be7c63c80b6375914be96c51ee1d090921
AddarshKumar123 <addarshkumar2005@gmail.com>
2024-06-25 21:55:32 +0530

    improved security in payment

--- client/cli/src/components/Adsformpage.jsx
+++ client/cli/src/components/Adsformpage.jsx
@@ -18,6 +18,7 @@ import axios from "axios";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { v4 as uuidv4 } from "uuid";
 import { imageDb } from "./firebaseauth/config";
+import Loginpage from "./Loginpage";
 
 const Adsformpage = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -39,8 +40,6 @@ const Adsformpage = () => {
       });
   }, []);
 
-  const checkoutHandler = async (amount, formData) => {
-    
   let token = "";
 
   const tokenCookie = document.cookie
@@ -52,6 +51,9 @@ const Adsformpage = () => {
   } else {
     console.error("Token cookie not found");
   }
+  
+  const checkoutHandler = async (amount, formData) => {
+    
     try {
       const { data: { order } } = await axios.post("https://s53-addarshkumar-capstone-bytebridge.onrender.com/checkout", { amount });
 
@@ -83,7 +85,6 @@ const Adsformpage = () => {
             duration: 9000,
             isClosable: true,
           });
-          console.log(formData);
           await axios.post("https://s53-addarshkumar-capstone-bytebridge.onrender.com/ad-data", formData, {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -167,61 +168,70 @@ const Adsformpage = () => {
   };
 
   return (
-    <HStack mt={"30px"} justifyContent={"center"}>
-      <VStack>
-        <Text mt={"20px"}>Choose The Number Of Images You Want To Show.</Text>
-
-        <HStack mt={"20px"} justifyContent={"center"}>
-          <Icon onClick={dec} as={FaChevronLeft} />
-          <Button colorScheme="blue">{totalImages}</Button>
-          <Icon onClick={inc} as={FaChevronRight} />
-        </HStack>
-
-        {Array.from({ length: totalImages }).map((_, index) => (
-          <FormControl key={index} mt="30px" isInvalid={errors[`image${index}`]}>
-            <FormLabel>Ad Image {index + 1}</FormLabel>
-            <Input
-              type="file"
-              aria-label={`Image ${index + 1}`}
-              {...register(`image${index}`, { required: `Image ${index + 1} is required` })}
-              onChange={(e) => handleImageChange(e, index)}
-            />
-            <FormErrorMessage>{errors[`image${index}`] && errors[`image${index}`].message}</FormErrorMessage>
-          </FormControl>
-        ))}
-
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormControl mt="30px" minW={["xs", "sm", "md", "md"]} isInvalid={errors.category}>
-            <FormLabel>Ad Category</FormLabel>
-            <Input type="text" aria-label="Ad category" {...register("category", { required: "Ad category is required" })} />
-            <FormErrorMessage>{errors.category && errors.category.message}</FormErrorMessage>
-          </FormControl>
-
-          <FormControl mt="30px" isInvalid={errors.description}>
-            <FormLabel>Ad Description</FormLabel>
-            <Input type="text" aria-label="Ad description" {...register("description", { required: "Ad description is required" })} />
-            <FormErrorMessage>{errors.description && errors.description.message}</FormErrorMessage>
-          </FormControl>
-
-          <FormControl mt="30px" isInvalid={errors.startDate}>
-            <FormLabel>Start Date</FormLabel>
-            <Input type="date" aria-label="Start date" {...register("startDate", { required: "Start date is required" })} />
-            <FormErrorMessage>{errors.startDate && errors.startDate.message}</FormErrorMessage>
-          </FormControl>
-
-          <FormControl mt="30px" isInvalid={errors.endDate}>
-            <FormLabel>End Date</FormLabel>
-            <Input type="date" aria-label="End date" {...register("endDate", { required: "End date is required" })} />
-            <FormErrorMessage>{errors.endDate && errors.endDate.message}</FormErrorMessage>
-          </FormControl>
-
-          <Button isLoading={isLoading} type="submit" mt="30px" colorScheme="blue">
-            Proceed
-          </Button>
-        </form>
-      </VStack>
-      <Image h={"80%"} w={"40vw"} src="https://fuse-client.vercel.app/About_Img/About.svg" />
-    </HStack>
+    <>
+    {
+      token.length>0?(
+        <HStack mt={"30px"} justifyContent={"center"}>
+        <VStack>
+          <Text mt={"20px"}>Choose The Number Of Images You Want To Show.</Text>
+  
+          <HStack mt={"20px"} justifyContent={"center"}>
+            <Icon onClick={dec} as={FaChevronLeft} />
+            <Button colorScheme="blue">{totalImages}</Button>
+            <Icon onClick={inc} as={FaChevronRight} />
+          </HStack>
+  
+          {Array.from({ length: totalImages }).map((_, index) => (
+            <FormControl key={index} mt="30px" isInvalid={errors[`image${index}`]}>
+              <FormLabel>Ad Image {index + 1}</FormLabel>
+              <Input
+                type="file"
+                aria-label={`Image ${index + 1}`}
+                {...register(`image${index}`, { required: `Image ${index + 1} is required` })}
+                onChange={(e) => handleImageChange(e, index)}
+              />
+              <FormErrorMessage>{errors[`image${index}`] && errors[`image${index}`].message}</FormErrorMessage>
+            </FormControl>
+          ))}
+  
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl mt="30px" minW={["xs", "sm", "md", "md"]} isInvalid={errors.category}>
+              <FormLabel>Ad Category</FormLabel>
+              <Input type="text" aria-label="Ad category" {...register("category", { required: "Ad category is required" })} />
+              <FormErrorMessage>{errors.category && errors.category.message}</FormErrorMessage>
+            </FormControl>
+  
+            <FormControl mt="30px" isInvalid={errors.description}>
+              <FormLabel>Ad Description</FormLabel>
+              <Input type="text" aria-label="Ad description" {...register("description", { required: "Ad description is required" })} />
+              <FormErrorMessage>{errors.description && errors.description.message}</FormErrorMessage>
+            </FormControl>
+  
+            <FormControl mt="30px" isInvalid={errors.startDate}>
+              <FormLabel>Start Date</FormLabel>
+              <Input type="date" aria-label="Start date" {...register("startDate", { required: "Start date is required" })} />
+              <FormErrorMessage>{errors.startDate && errors.startDate.message}</FormErrorMessage>
+            </FormControl>
+  
+            <FormControl mt="30px" isInvalid={errors.endDate}>
+              <FormLabel>End Date</FormLabel>
+              <Input type="date" aria-label="End date" {...register("endDate", { required: "End date is required" })} />
+              <FormErrorMessage>{errors.endDate && errors.endDate.message}</FormErrorMessage>
+            </FormControl>
+  
+            <Button isLoading={isLoading} type="submit" mt="30px" colorScheme="blue">
+              Proceed
+            </Button>
+          </form>
+        </VStack>
+        <Image h={"80%"} w={"40vw"} src="https://fuse-client.vercel.app/About_Img/About.svg" />
+      </HStack>
+      ):(
+        <Loginpage />
+      )
+    }
+    </>
+    
   );
 };
 
