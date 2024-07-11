bcf20892b9a919cc7ad88e87ee1ef04e2d580125
AddarshKumar123 <addarshkumar2005@gmail.com>
2024-07-11 07:50:14 +0530

    added regex in password field

--- Backend/validator/uservalidation.js
+++ Backend/validator/uservalidation.js
@@ -3,7 +3,10 @@ const joi=require("joi");
 const userValidator= joi.object({
     name:joi.string().min(3).max(20).required(),
     email:joi.string().email().required(),
-    password:joi.string().pattern(new RegExp('^(?=.*[a-zA-Z])(?=.*[^a-zA-Z\d\s])(.{6,30})$')),
+    password: joi.string().pattern(new RegExp('^(?=.*[a-zA-Z])(?=.*[^a-zA-Z\d\s]).{6,30}$')).required().messages({
+        'string.pattern.base': 'Password must be 6-30 characters long, contain at least one alphabet',
+        'string.empty': 'Password is required'
+      }),
     profileImg:joi.string()
 })
 
